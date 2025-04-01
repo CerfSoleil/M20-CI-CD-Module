def6e2d8e034e857e39fcbc1a9a09144da0ecbc5
CerfSoleil <robedj12@gmail.com>
2025-04-01 15:17:07 -0500

    script update

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,8 +3,14 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
-      name: collectionName
+    const model = models[modelName];
+
+    if (!model.db || !model.db.db) {
+      console.error(`Model has no database connection.`);
+      return;
+    }
+    const modelExists = await
+    model.db.db.listCollections({ name: collectionName
     }).toArray()
 
     if (modelExists.length) {
